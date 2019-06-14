647fcc13ab4a5102a2c012a18f44fa98323aec28
WilliamVoong <44473866+WilliamVoong@users.noreply.github.com>
2019-06-14 22:46:51 +0200

    The gui implementation
    To expand on the previous idea, we use JFRAME and JTABLE to create a gui implementation of the current design.

---
+++ Jtable -matkostnader (Jtable)/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.awt.*;
+import javax.swing.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        table gui= new table();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setSize(750,600);
+        gui.setVisible(true);
+        gui.setTitle("Food cost");
+	// write your code here
+    }
+}
